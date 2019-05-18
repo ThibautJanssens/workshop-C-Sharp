fd38d4f3a7768a9c9aed3f0e18c656977ffdb38d
ThibautJanssens <janssensthib@gmail.com>
2019-05-18 13:29:23 +0200

    fixing minor bug and improving ex magic number

--- Solutions/magicNumber.cs
+++ Solutions/magicNumber.cs
@@ -1,61 +1,60 @@
 ﻿using System;
 
-namespace Algo
-{
-    class MainClass
-    {
+namespace Workshop {
+    class Program {
         private static Random random = new Random();
 
-        public static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             //variables
-            bool play = true;
+            bool play = true, victory = false;
             int magicNbr, nbrOfTry, choice;
 
             Console.WriteLine("Welcome to this game of the magic number.");
-
             //game
-            do
-            {
+            do {
                 Console.WriteLine("I choosed a number between 1 and 100.");
                 Console.WriteLine("Let's see if you can find it.");
                 magicNbr = random.Next(1, 101);
                 nbrOfTry = 0;
 
-                //while the user is still trying to find the number
-                do
-                {
-                    choice = Convert.ToInt32(Console.ReadLine());
-                    nbrOfTry++;
+                //prevent error if the user is trying to enter a string instead of a number
+                try {
+                    //while the user is still trying to find the number
+                    do {
+                        choice = Convert.ToInt32(Console.ReadLine());
+                        nbrOfTry++;
 
-                    if (choice < magicNbr)
-                    {
-                        Console.WriteLine("My number is greater than yours.");
-                    }
-                    else if(choice > magicNbr)
-                    {
-                        Console.WriteLine("My number is lower than yours.");    
-                    }
-                } 
-                while (choice != magicNbr); //user found the number
-
-                Console.WriteLine("Congratulations! You found the number in {0} rounds.", nbrOfTry);
-                Console.WriteLine("Do you want to continue playing? Y/N.");
-                String endChoice = Console.ReadLine();
-
-                while (endChoice != "Y" && endChoice != "y" && endChoice == "N" && endChoice == "n")
-                {
-                    Console.WriteLine("Not a valid choice. Press Y or N");
+                        if (choice < magicNbr) {
+                            Console.WriteLine("My number is greater than yours.");
+                        }
+                        else if (choice > magicNbr) {
+                            Console.WriteLine("My number is lower than yours.");
+                        }
+                    } while (choice != magicNbr);
+                    //user found the magic number
+                    victory = true;
+                }
+                //user tried to enter a string instead of numbers
+                catch (Exception ex) {
+                    Console.WriteLine("Not a valid number\nYou tried something different, the game will restart.");
+                }
+                if (victory) {
+                    Console.WriteLine("Congratulations! You found the number in {0} rounds.", nbrOfTry);
                     Console.WriteLine("Do you want to continue playing? Y/N.");
-                    endChoice = Console.ReadLine();
+                    String endChoice = Console.ReadLine();
+
+                    while (endChoice != "Y" && endChoice != "y" && endChoice == "N" && endChoice == "n") {
+                        Console.WriteLine("Not a valid choice. Press Y or N");
+                        Console.WriteLine("Do you want to continue playing? Y/N.");
+                        endChoice = Console.ReadLine();
+                    }
+                    if (endChoice == "N" || endChoice == "n") {
+                        play = false;
+                    }
                 }
-                if(endChoice == "N" || endChoice == "n")
-                {
-                    play = false;
-                }
-            } 
-            while (play); //end of game
+            } while (play); //end of game
             Console.WriteLine("Goodbye, thanks for playing.");
         }
+
     }
 }
